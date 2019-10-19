81afb0b0a6ecf04e672db7727f1e00bfde93dce4
Iain <iain@polevaultweb.com>
2019-10-19 21:00:31 +0100

    Add grunt build process.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function( grunt ) {
+
+	// load all grunt tasks in package.json matching the `grunt-*` pattern
+	require( 'load-grunt-tasks' )( grunt );
+
+	// Project configuration
+	grunt.initConfig( {
+		pkg:    grunt.file.readJSON( 'package.json' ),
+		clean: {
+			main: ['release']
+		},
+		copy: {
+			// Copy the plugin to a versioned release directory
+			main: {
+				src:  [
+					'**',
+					'!node_modules/**',
+					'!release/**',
+					'!.git/**',
+					'!.sass-cache/**',
+					'!css/src/**',
+					'!js/src/**',
+					'!img/src/**',
+					'!Gruntfile.js',
+					'!package.json',
+					'!.gitignore',
+					'!.gitmodules',
+					'!yarn.lock',
+					'!composer.lock',
+					'!composer.json'
+				],
+				dest: 'release/<%= pkg.version %>/'
+			}
+		},
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: './release/<%= pkg.name %>.zip'
+				},
+				expand: true,
+				cwd: 'release/<%= pkg.version %>/',
+				src: ['**/*'],
+				dest: '<%= pkg.name %>/'
+			}
+		},
+	} );
+
+	grunt.registerTask( 'build', ['clean', 'copy', 'compress'] );
+};
